a46e54228e535f0bbb920085433483bd11d2c274
Bertrand Kintanar <bertrand.kintanar@gmail.com>
2025-02-15 06:45:22 +1300

    added working api/customers endpoint.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+
 abstract class Controller
 {
-    //
+    protected const ITEMS_PER_PAGE = 15;
+
+    protected int $perPage = self::ITEMS_PER_PAGE;
+
+    public function __construct(Request $request)
+    {
+        $this->perPage = $request->per_page ?? self::ITEMS_PER_PAGE;
+    }
 }
--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -1,10 +1,19 @@
 <?php
 
 namespace App\Http\Controllers;
+
+use App\Models\Customer;
+use Illuminate\Http\Request;
+use Spatie\QueryBuilder\QueryBuilder;
+
 class CustomerController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-
+        return QueryBuilder::for(Customer::class)
+            ->allowedFilters(['first_name', 'last_name', 'email', 'company', 'title'])
+            ->allowedSorts('first_name', 'last_name')
+            ->paginate($this->perPage)
+            ->appends($request->query());
     }
 }
